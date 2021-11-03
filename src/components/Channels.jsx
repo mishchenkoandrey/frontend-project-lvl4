7ed57a402a73259a22040708edae2610e8c453be
Andrey Mishchenko <buryonok@gmail.com>
2021-11-03 16:39:34 +0300

    Add channels add block

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -2,49 +2,10 @@
 
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  Nav, Button, Dropdown, ButtonGroup,
-} from 'react-bootstrap';
+import { Button, Nav } from 'react-bootstrap';
 import { setCurrentChannelId } from '../slices/channelsSlice.js';
-
-const DefaultChannel = ({ name, btnVariant, handleActiveChannel }) => (
-  <Nav.Item className="w-100" as="li">
-    <Button
-      variant={btnVariant}
-      onClick={handleActiveChannel}
-      className="w-100 rounded-0 text-start"
-    >
-      <span className="me-1">#</span>
-      {name}
-    </Button>
-  </Nav.Item>
-);
-
-const ControlledChannel = ({
-  name,
-  btnVariant,
-  removeChannel,
-  renameChannel,
-  handleActiveChannel,
-}) => (
-  <Nav.Item className="w-100" as="li">
-    <Dropdown as={ButtonGroup} className="d-flex mb-2">
-      <Button
-        onClick={handleActiveChannel}
-        variant={btnVariant}
-        className="flex-grow-1 w-100 rounded-0 text-start"
-      >
-        <span className="me-1">#</span>
-        {name}
-      </Button>
-      <Dropdown.Toggle split variant={btnVariant} className="flex-grow-0" />
-      <Dropdown.Menu>
-        <Dropdown.Item onClick={removeChannel}>remove</Dropdown.Item>
-        <Dropdown.Item onClick={renameChannel}>rename</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  </Nav.Item>
-);
+import DefaultChannel from './DefaultChannel.jsx';
+import ControlledChannel from './ControlledChannel.jsx';
 
 const Channels = () => {
   const channels = useSelector((state) => state.channelsInfo.channels);
@@ -55,6 +16,18 @@ const Channels = () => {
 
   return (
     <>
+      <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
+        <span>channels</span>
+        <Button
+          variant="link"
+          className="p-0 text-primary btn-group-vertical"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+          </svg>
+        </Button>
+      </div>
       <Nav className="flex-column nav-pills nav-fill px-2" as="ul">
         {channels.map(({ id, name, removable }) => {
           const btnVariant = id === currentChannelId && 'secondary';
---
+++ src/components/ControlledChannel.jsx
@@ -0,0 +1,34 @@
+// @ts-check
+
+import React from 'react';
+import {
+  Nav, Button, Dropdown, ButtonGroup,
+} from 'react-bootstrap';
+
+const ControlledChannel = ({
+  name,
+  btnVariant,
+  removeChannel,
+  renameChannel,
+  handleActiveChannel,
+}) => (
+  <Nav.Item className="w-100" as="li">
+    <Dropdown as={ButtonGroup} className="d-flex mb-2">
+      <Button
+        onClick={handleActiveChannel}
+        variant={btnVariant}
+        className="w-100 rounded-0 text-start text-truncate"
+      >
+        <span className="me-1">#</span>
+        {name}
+      </Button>
+      <Dropdown.Toggle split variant={btnVariant} className="flex-grow-0" />
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={removeChannel}>remove</Dropdown.Item>
+        <Dropdown.Item onClick={renameChannel}>rename</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  </Nav.Item>
+);
+
+export default ControlledChannel;
