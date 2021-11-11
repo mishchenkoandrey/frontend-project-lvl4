8783a904d7d5787fffc783973cb838ba72b5fbba
Andrey Mishchenko <buryonok@gmail.com>
2021-11-12 00:27:42 +0300

    Update message form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -34,13 +34,13 @@ const MessageForm = () => {
       } catch (error) {
         setErrors({ body: error.message });
       }
-      console.log(formik.isSubmitting);
       setTimeout(() => {
         formik.setSubmitting(false);
-        console.log(formik);
       }, 3000);
     },
   });
+
+  console.log(formik);
 
   useEffect(() => {
     inputRef.current.focus();
@@ -67,7 +67,7 @@ const MessageForm = () => {
           disabled={formik.isSubmitting}
         />
         <InputGroup.Append>
-          <Button type="submit" variant="group-vertical" disabled={!formik.isValid}>
+          <Button type="submit" variant="group-vertical" disabled={!formik.isValid || !formik.dirty}>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-right-square" viewBox="0 0 16 16">
               <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
             </svg>
