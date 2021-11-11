1fcd8640805a1bd675ef1d1b75b878972f465d37
Andrey Mishchenko <buryonok@gmail.com>
2021-11-11 14:53:37 +0300

    Update message form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -16,10 +16,6 @@ const MessageForm = () => {
   const currentChannelId = useSelector((state) => state.channelsInfo.currentChannelId);
   const inputRef = useRef(null);
 
-  useEffect(() => {
-    inputRef.current.focus();
-  }, [currentChannelId]);
-
   const formik = useFormik({
     initialValues: {
       body: '',
@@ -40,7 +36,13 @@ const MessageForm = () => {
       }
     },
   });
-  console.log(formik.isSubmitting, !formik.isValid, !formik.dirty);
+
+  useEffect(() => {
+    if (!formik.isSubmitting) {
+      inputRef.current.focus();
+    }
+  }, [formik.isSubmitting]);
+
   return (
     <Form
       noValidate
@@ -59,9 +61,10 @@ const MessageForm = () => {
           data-testid="new-message"
           placeholder="Введите сообщение..."
           className="border-0 p-0 ps-2"
+          disabled={formik.isSubmitting}
         />
         <InputGroup.Append>
-          <Button type="submit" variant="group-vertical" disabled={formik.isSubmitting || !formik.isValid || !formik.dirty}>
+          <Button type="submit" variant="group-vertical" disabled={!formik.isValid || !formik.dirty}>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-right-square" viewBox="0 0 16 16">
               <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
             </svg>
