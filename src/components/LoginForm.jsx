5fb19c39ea5a156ad82d627d903f2dad08f2b635
Andrey Mishchenko <buryonok@gmail.com>
2021-11-16 16:39:07 +0300

    Remove login form to LogimForm.jsx

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,94 @@
+// @ts-check
+
+import axios from 'axios';
+import React, { useEffect, useRef, useState } from 'react';
+import { useFormik } from 'formik';
+import { Button, Form } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import useAuth from '../hooks/useAuth.js';
+import routes from '../routes.js';
+import validationSchemas from '../validation.js';
+
+const LoginForm = () => {
+  const auth = useAuth();
+  const [isAuthFailed, setIsAuthFailed] = useState(false);
+  const inputRef = useRef();
+  const history = useHistory();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: validationSchemas.LoginFormSchema,
+    onSubmit: async (loginData) => {
+      try {
+        const response = await axios.post(routes.loginPath(), loginData);
+        const { token, username } = response.data;
+        auth.logIn(token, username);
+        history.replace('/');
+      } catch (error) {
+        if (!error.isAxiosError || error.response.status !== 401) {
+          throw new Error(error);
+        }
+        setIsAuthFailed(true);
+        inputRef.current.select();
+      }
+    },
+  });
+
+  return (
+    <Form onSubmit={formik.handleSubmit} className="mt-3 mt-mb-0">
+      <h1 className="text-center mb-4">Войти</h1>
+      <Form.Group className="form-floating mb-3">
+        <Form.Control
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.username}
+          placeholder="Ваш ник"
+          name="username"
+          id="username"
+          autoComplete="username"
+          isInvalid={isAuthFailed
+            || (formik.touched.username && !!formik.errors.username)}
+          required
+          ref={inputRef}
+          className="form-control"
+          disabled={formik.isSubmitting}
+        />
+        <Form.Label htmlFor="username">Ваш ник</Form.Label>
+        {formik.touched.username && formik.errors.username ? (
+          <Form.Control.Feedback type="invalid">{formik.errors.username}</Form.Control.Feedback>
+        ) : null}
+      </Form.Group>
+      <Form.Group className="form-floating mb-4">
+        <Form.Control
+          type="password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+          placeholder="password"
+          name="password"
+          id="password"
+          autoComplete="current-password"
+          isInvalid={isAuthFailed
+            || (formik.touched.password && !!formik.errors.password)}
+          required
+          className="form-control"
+          disabled={formik.isSubmitting}
+        />
+        <Form.Label htmlFor="password">Password</Form.Label>
+        {formik.touched.password && formik.errors.password ? (
+          <Form.Control.Feedback type="invalid">{formik.errors.password}</Form.Control.Feedback>
+        ) : null}
+        <Form.Control.Feedback type="invalid">the username or password is incorrect</Form.Control.Feedback>
+      </Form.Group>
+      <Button type="submit" variant="outline-primary" className="w-100 mb-3" disabled={formik.isSubmitting}>Submit</Button>
+    </Form>
+  );
+};
+
+export default LoginForm;
