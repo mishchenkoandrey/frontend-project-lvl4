746f19dfd4e5444dd1496560073df6561a54cb55
Andrey Mishchenko <buryonok@gmail.com>
2021-12-18 20:13:32 +0300

    Update AddingPanel.jsx

--- src/components/AddingPanel.jsx
+++ src/components/AddingPanel.jsx
@@ -1,13 +1,92 @@
 // @ts-check
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { useFormik } from 'formik';
+import { Form, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 import useSocket from '../hooks/useSocket.js';
 
-import ChannelForm from './ChannelForm.jsx';
+import validationSchemas from '../validation.js';
+// import ChannelForm from './ChannelForm.jsx';
 
-const AddingPanel = ({ closeModal }) => {
+const AddingPanel = ({
+  closeModal,
+}) => {
+  console.log(closeModal);
+  const socket = useSocket();
+  const { channelFormSchema } = validationSchemas();
+  const { t } = useTranslation();
+  const notify = (message) => toast(message);
+  const inputRef = useRef();
+  const channels = useSelector((state) => state.channelsInfo.channels);
+  const channelsNames = channels.map(({ name }) => name);
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+    },
+    validationSchema: channelFormSchema(channelsNames),
+    onSubmit: ({ name }, { setErrors }) => {
+      const channel = { name };
+      try {
+        socket.addChannel(channel);
+        closeModal();
+        notify(t('channelCreated'));
+      } catch (error) {
+        if (error.message === 'networkError') {
+          setTimeout(() => {
+            formik.setSubmitting(false);
+          }, 3000);
+        }
+        setErrors({ name: error.message });
+      }
+    },
+    validateOnChange: false,
+  });
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [formik.isSubmitting]);
+
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Group>
+        <Form.Control
+          value={formik.values.name}
+          ref={inputRef}
+          onFocus={(e) => e.currentTarget.select()}
+          type="text"
+          name="name"
+          id="name"
+          onChange={formik.handleChange}
+          aria-label="add channel"
+          className="mb-2 form-control"
+          data-testid="add-channel"
+          isInvalid={!formik.isValid}
+          autoComplete="off"
+        />
+        <Form.Control.Feedback type="invalid">
+          {(formik.errors.name !== 'networkError') && formik.errors.name}
+        </Form.Control.Feedback>
+        <div className="d-flex justify-content-end">
+          <Button
+            disabled={formik.isSubmitting}
+            onClick={closeModal}
+            type="button"
+            variant="secondary"
+            className="me-2"
+          >
+            {t('cancel')}
+          </Button>
+          <Button type="submit" variant="primary" disabled={formik.isSubmitting}>{t('send')}</Button>
+        </div>
+      </Form.Group>
+    </Form>
+  );
+};
+
+/* const AddingPanel = ({ closeModal }) => {
   const socket = useSocket();
   const initialName = '';
   const { t } = useTranslation();
@@ -26,11 +105,9 @@ const AddingPanel = ({ closeModal }) => {
 
   return (
     <ChannelForm
-      initialName={initialName}
-      handleSubmit={addChannel}
       closeModal={closeModal}
     />
   );
-};
+}; */
 
 export default AddingPanel;
