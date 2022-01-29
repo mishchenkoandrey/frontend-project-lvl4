0c4cfead0ce5217556c95c01c0a4bddcdb3fa63a
Andrey Mishchenko <buryonok@gmail.com>
2022-01-29 14:25:01 +0300

    Update error processing

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,6 +5,7 @@ import React, { useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import routes from '../routes.js';
@@ -18,7 +19,8 @@ const HomePage = () => {
   const dispatch = useDispatch();
   const auth = useAuth();
   const { t } = useTranslation();
-  const notify = (error) => toast.error(t(error));
+  const history = useHistory();
+  const notify = () => toast.error(t('networkError'));
 
   const fetchData = async () => {
     const token = auth.getToken();
@@ -33,17 +35,12 @@ const HomePage = () => {
       const { data } = response;
       dispatch(initChannels({ data }));
     } catch (error) {
-      if (error.isAxiosError && error.response.status === 401) {
-        auth.logOut();
-        return;
+      if (!error.isAxiosError || error.response.status !== 401) {
+        notify();
+        throw new Error(error);
       }
-      if (error.isAxiosError && error.response.status === 500) {
-        notify('networkError');
-        console.error(error.response.statusText);
-        return;
-      }
-      notify('unknownError');
-      console.error(error.response.statusText);
+
+      history.replace('/login');
     }
   };
 
