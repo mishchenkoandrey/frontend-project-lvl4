2c24289835631da837f001724caf41f267aa2a2d
mishchenkoandrey <buryonok@gmail.com>
2023-03-23 19:39:58 +0600

    Move useEffect to LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -18,10 +18,6 @@ const [isAuthFailed, setIsAuthFailed] = useState(false);
 const inputRef = useRef();
 const history = useHistory();
 
-useEffect(() => {
-  inputRef.current.focus();
-}, []);
-
 const formik = useFormik({
   initialValues: {
     username: '',
@@ -45,61 +41,67 @@ const formik = useFormik({
   },
 });
 
-const LoginForm = () => (
-  <Form onSubmit={formik.handleSubmit} className="mt-3 mt-mb-0">
-    <h1 className="text-center mb-4">{t('login')}</h1>
-    <Form.Group className="form-floating mb-3">
-      <Form.Control
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.username}
-        placeholder={t('yourNick')}
-        name="username"
-        id="username"
-        autoComplete="username"
-        isInvalid={isAuthFailed
-          || (formik.touched.username && !!formik.errors.username)}
-        required
-        ref={inputRef}
-        disabled={formik.isSubmitting}
-      />
-      <Form.Label htmlFor="username">{t('yourNick')}</Form.Label>
-      {!isAuthFailed && formik.touched.username && formik.errors.username && (
-        <Form.Control.Feedback type="invalid">
-          {formik.errors.username}
-        </Form.Control.Feedback>
-      )}
-    </Form.Group>
-    <Form.Group className="form-floating mb-4">
-      <Form.Control
-        type="password"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.password}
-        placeholder={t('password')}
-        name="password"
-        id="password"
-        autoComplete="current-password"
-        isInvalid={isAuthFailed
-          || (formik.touched.password && !!formik.errors.password)}
-        required
-        disabled={formik.isSubmitting}
-      />
-      <Form.Label htmlFor="password">{t('password')}</Form.Label>
-      {!isAuthFailed && formik.touched.password && formik.errors.password && (
-        <Form.Control.Feedback type="invalid">
-          {formik.errors.password}
-        </Form.Control.Feedback>
-      )}
-      {isAuthFailed
-        && (
-        <Form.Control.Feedback type="invalid">
-          {t('invalidUsernameOrPassword')}
-        </Form.Control.Feedback>
+const LoginForm = () => {
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <Form onSubmit={formik.handleSubmit} className="mt-3 mt-mb-0">
+      <h1 className="text-center mb-4">{t('login')}</h1>
+      <Form.Group className="form-floating mb-3">
+        <Form.Control
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.username}
+          placeholder={t('yourNick')}
+          name="username"
+          id="username"
+          autoComplete="username"
+          isInvalid={isAuthFailed
+            || (formik.touched.username && !!formik.errors.username)}
+          required
+          ref={inputRef}
+          disabled={formik.isSubmitting}
+        />
+        <Form.Label htmlFor="username">{t('yourNick')}</Form.Label>
+        {!isAuthFailed && formik.touched.username && formik.errors.username && (
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.username}
+          </Form.Control.Feedback>
         )}
-    </Form.Group>
-    <Button type="submit" variant="outline-primary" className="w-100 mb-3" disabled={formik.isSubmitting}>{t('login')}</Button>
-  </Form>
-);
+      </Form.Group>
+      <Form.Group className="form-floating mb-4">
+        <Form.Control
+          type="password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+          placeholder={t('password')}
+          name="password"
+          id="password"
+          autoComplete="current-password"
+          isInvalid={isAuthFailed
+            || (formik.touched.password && !!formik.errors.password)}
+          required
+          disabled={formik.isSubmitting}
+        />
+        <Form.Label htmlFor="password">{t('password')}</Form.Label>
+        {!isAuthFailed && formik.touched.password && formik.errors.password && (
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.password}
+          </Form.Control.Feedback>
+        )}
+        {isAuthFailed
+          && (
+          <Form.Control.Feedback type="invalid">
+            {t('invalidUsernameOrPassword')}
+          </Form.Control.Feedback>
+          )}
+      </Form.Group>
+      <Button type="submit" variant="outline-primary" className="w-100 mb-3" disabled={formik.isSubmitting}>{t('login')}</Button>
+    </Form>
+  );
+};
 
 export default LoginForm;
